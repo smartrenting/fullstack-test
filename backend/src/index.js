4d00451de5bf2227246414508cab3a7c3104c026
Alexandre Kohen <alexandre@kohen.fr>
2018-06-20 12:34:48 +0200

    Updated for GAE

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,13 @@ import resolvers from './resolvers';
 import typeDefs from './schema';
 import { properties, clients } from './data';
 
-const server = new ApolloServer({ typeDefs, resolvers, context: { properties, clients } });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: { properties, clients },
+});
 
-server.listen().then(({ url }) => {
+const port = process.env.PORT || 8080;
+server.listen({ http: { port } }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
